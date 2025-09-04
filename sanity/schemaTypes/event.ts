29423f453d404c9de9fd6f320ffe96e910b95b11
Muhammad Anas Qadri <161422002+Anas-Rajput12@users.noreply.github.com>
2025-09-04 15:29:03 +0500

    Delete sanity directory

--- sanity/schemaTypes/event.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  name: 'event',
-  title: 'Events',
-  type: 'document',
-  fields: [
-    { name: 'title', type: 'string', title: 'Event Title' },
-    { name: 'date', type: 'string', title: 'Event Date' },
-  ]
-}
