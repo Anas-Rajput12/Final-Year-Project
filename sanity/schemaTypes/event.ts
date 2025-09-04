e1a1c5b152fad74b2d7e86d65b0dee550669590f
Muhammad Anas Qadri <161422002+Anas-Rajput12@users.noreply.github.com>
2025-09-04 18:44:50 +0500

    Add files via upload

---
+++ sanity/schemaTypes/event.ts
@@ -0,0 +1,9 @@
+export default {
+  name: 'event',
+  title: 'Events',
+  type: 'document',
+  fields: [
+    { name: 'title', type: 'string', title: 'Event Title' },
+    { name: 'date', type: 'string', title: 'Event Date' },
+  ]
+}
---
+++ sanity/schemaTypes/timetable.ts
@@ -0,0 +1,11 @@
+export default {
+  name: 'timetable',
+  title: 'Class Timetable',
+  type: 'document',
+  fields: [
+    { name: 'course', type: 'string', title: 'Course' },
+    { name: 'day', type: 'string', title: 'Day' },
+    { name: 'time', type: 'string', title: 'Time' },
+    { name: 'room', type: 'string', title: 'Room' },
+  ]
+}
